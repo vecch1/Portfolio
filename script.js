610af663d3ca0034510c8a162ab513102b20da7a
Lucas Vecchi Barcelos <lucasvecchibarcelos@gmail.com>
2025-03-18 12:15:04 -0300

    wpp contato

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-const nome = document.getElementById("nome");
+function enviarWhatsapp(event){
+
+    event.preventDefault();
+
+
+    const nome = document.getElementById("nome").value;
+    const mensagem = document.getElementById("mensagem").value;
+    const telefone = '5511975615269';
+
+    const texto = `Olá! Me chamo ${nome}, ${mensagem}`;
+    const mensagemFormatada = encodeURIComponent(texto);
+
+    const url = `https://whatsa.me/${telefone}/?t=${mensagemFormatada}`;
+
+    window.open(url, '_blank');
+}
+
+
+
